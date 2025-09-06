76b131a94ef5adcbe1247d639b94cec813f37668
Abhishek Pandya <abhishekpandya5@gmail.com>
2025-09-07 00:41:07 +0530

    FE fixes

--- FRONTEND/src/RootLayout.jsx
+++ FRONTEND/src/RootLayout.jsx
@@ -1,7 +1,3 @@
-import React from "react";
-import HomePage from "./pages/HomePage";
-import LoginForm from "./components/LoginForm";
-import AuthPage from "./pages/AuthPage";
 import { Outlet } from "@tanstack/react-router";
 import Navbar from "./components/NavBar";
 
